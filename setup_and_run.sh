455ed9a04991889e9b36907afcc1b67c9aafc6e1
Nebtyy <123236820+Nebtyy@users.noreply.github.com>
2024-11-12 15:30:23 +0100

    Update setup_and_run.sh

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -3,7 +3,7 @@
 # Updating the system and installing the necessary utilities
 echo "Updating the system and installing the necessary utilities..."
 sudo apt-get update
-sudo apt-get install -y curl jq git python3 python3-pip wget linkchecker
+sudo apt-get install -y curl jq git python3 python3-pip wget linkchecker python3-venv xsltproc nmap dnsrecon
 
 # Install Go if it is not installed
 if ! command -v go &> /dev/null; then
@@ -24,9 +24,10 @@ go install github.com/lc/subzy@latest
 go install github.com/tomnomnom/waybackurls@latest
 go install github.com/lc/gau@latest
 go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install github.com/michenriksen/aquatone@latest  # Install aquatone
 
 # Installing Python libraries from requirements.txt
-echo "Installing Python libraries..."
+echo "Installing Python libraries from requirements.txt..."
 pip install -r requirements.txt
 
 # Installing SecretFinder
@@ -34,8 +35,17 @@ echo "Installing SecretFinder..."
 git clone https://github.com/m4ll0k/SecretFinder.git ~/Downloads/secretfinder
 pip install -r ~/Downloads/secretfinder/requirements.txt
 
-# Setting up linkchecker
-echo "Installing linkchecker..."
-sudo apt-get install -y linkchecker
+# Ensuring all necessary Python libraries are installed
+echo "Ensuring additional Python libraries are installed..."
+pip install --upgrade pip
+pip install beautifulsoup4 lxml html5lib selenium uro subprocess requests pytest
+
+# Verify installation of aquatone
+if ! command -v aquatone &> /dev/null; then
+    echo "Error: aquatone installation failed."
+    exit 1
+else
+    echo "Aquatone installed successfully."
+fi
 
 echo "All dependencies installed successfully!"
