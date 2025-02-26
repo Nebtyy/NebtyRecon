ba6378d1bce0383db18618e1df9c1692d0b02190
Nebtyy <123236820+Nebtyy@users.noreply.github.com>
2025-02-26 10:18:48 +0100

    Update setup_and_run.sh

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -36,43 +36,12 @@ go install github.com/hakluke/hakrawler@latest
 mkdir -p ./MyTools
 cd MyTools
 
-#!/bin/bash
-
-# Updating the system and installing the necessary utilities
-echo "Updating the system and installing the necessary utilities..."
-sudo apt-get update
-sudo apt-get install -y curl jq git python3 python3-pip wget linkchecker python3-venv xsltproc nmap dnsrecon
-
-# Install Go if it is not installed
-if ! command -v go &> /dev/null; then
-    echo "Installing Go..."
-    wget https://dl.google.com/go/go1.20.2.linux-amd64.tar.gz
-    sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
-    export PATH=$PATH:/usr/local/go/bin
-    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-    source ~/.profile
-fi
-
-# Ensure Go environment paths are set
-export PATH=$PATH:/usr/local/go/bin
-export PATH=$PATH:$(go env GOPATH)/bin
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.profile
-
-# Installing Go utilities
-echo "Installing Go utilities..."
-go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-go install github.com/tomnomnom/assetfinder@latest
-go install -v github.com/OWASP/Amass/v3/...@latest
-go install github.com/lc/subzy@latest
-go install github.com/lc/gau@latest
-go install github.com/projectdiscovery/katana/cmd/katana@latest
-go install github.com/michenriksen/aquatone@latest
-go install github.com/projectdiscovery/httpx/cmd/httpx@latest
-go install github.com/hakluke/hakrawler@latest
-
-mkdir -p ./MyTools
-cd MyTools
+# Installing Aquatone
+mkdir Aquatone
+cd Aquatone
+wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
+unzip aquatone_linux_amd64_1.7.0.zip
+cd ..
 
 # Installing SecretFinder
 echo "Installing SecretFinder..."
@@ -118,13 +87,5 @@ fi
 echo "Ensuring additional Python libraries are installed..."
 pip install --upgrade pip
 pip install beautifulsoup4 lxml html5lib selenium uro subprocess requests pytest
-
-# Verify installation of aquatone
-if ! command -v aquatone &> /dev/null; then
-    echo "Error: aquatone installation failed."
-    exit 1
-else
-    echo "Aquatone installed successfully."
-fi
 
 echo "All dependencies installed successfully!"
